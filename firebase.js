6685727502f1ec9df0eec4758962425181bc5550
mrajarshi570 <mrajarshi570@gmail.com>
2025-09-03 07:12:03 +0000

    firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+// For more information on how to get this value visit: https://firebase.google.com/docs/web/setup#config-object
+const firebaseConfig = {
+      apiKey: "AIzaSyAThDQpmTEP_7UVjjqN-MA9A5ViRom7mw8",
+          authDomain: "donate-6f850.firebaseapp.com",
+              projectId: "donate-6f850",
+                  
+                      messagingSenderId: "805870053914",
+                          appId: "1:805870053914:web:9d7472f3f57432ef19bfa3"
+                            };
+}
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
